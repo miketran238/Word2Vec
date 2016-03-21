ee709cc92ae1a1e1cf7122efe72751b6dc73fbb0
thanhnguyen12 <thanhng.cs@gmail.com>
2016-03-21 16:16:30 -0500

    Update api mappings

--- Word2VecJava-master/src/main/java/edu/iastate/ece/api2vec/APIMappings.java
+++ Word2VecJava-master/src/main/java/edu/iastate/ece/api2vec/APIMappings.java
@@ -2,15 +2,43 @@ package edu.iastate.ece.api2vec;
 
 import java.io.File;
 import java.util.LinkedHashMap;
+import java.util.List;
 
+import com.jujutsu.utils.MatrixUtils;
+import com.medallia.word2vec.Searcher.Match;
 import com.medallia.word2vec.Word2VecModel;
 import com.medallia.word2vec.Word2VecProcessingJava;
 import com.medallia.word2vec.util.FileUtils;
+import com.medallia.word2vec.util.Strings;
 
 public class APIMappings {
 	
 	public static void main(String[] args) {
-		APIMappings.subsample(100);
+		APIMappings mapper = new APIMappings();
+		
+//		APIMappings.subsample(100);
+		
+		mapper.doJDKApacheMapping(100);
+	}
+	
+	public void doJDKApacheMapping(int numbSamples) {
+		LinkedHashMap<String, Integer> APIListWithHighestFreq = getJDKAPIVocabulary();
+		
+		Word2VecModel model = null;
+		try {
+			model = Word2VecModel.fromBinFile(new File("text8_annot.bin"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		int smplCount = 0;
+		for(String API : APIListWithHighestFreq.keySet()) {
+			if(smplCount ++ < numbSamples) {
+				List<Match> matches = Word2VecProcessingJava.getMappedAPIs(API, model);
+				if(matches != null)
+					System.out.println(API + "\n" +Strings.joinObjects("\n", matches));
+			}
+		}
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -74,6 +102,30 @@ public class APIMappings {
 				return;
 		}
 		
+	}
+	
+	public LinkedHashMap<String, Integer> getJDKAPIVocabulary() {
+		String [] apiListWithFreq = MatrixUtils.simpleReadLines(new File("JDKAPIs.txt"));
+		
+		LinkedHashMap<String, Integer> APIListWithHighestFreq = new LinkedHashMap<String, Integer>();
+		
+		for(String record : apiListWithFreq) {
+			String [] fields = record.split("\t");
+			if(fields.length == 3) {
+				String type = fields[0];
+				String fQualifiedName = fields[1];
+				Integer freq = Integer.parseInt(fields[2]);
+				
+				if(type.equals("method") && !fQualifiedName.contains("new")) {
+					String[] names = fQualifiedName.split("\\.");
+					int length = names.length;
+					String fmtAPI = "jdk::" + names[length-2] + "::" + names[length-1];
+					APIListWithHighestFreq.put(fmtAPI, freq);
+				}
+			}
+		}
+		
+		return APIListWithHighestFreq;
 	}
 	
 	public static void mapAPIs() {
